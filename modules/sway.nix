fc9a2150ed7f66b80df887d425023b7dc3bfc54a
Sean Wang <seanwang313@outlook.com>
2025-10-13 14:46:02 +0200

    fixed.

--- modules/sway.nix
+++ modules/sway.nix
@@ -15,7 +15,7 @@
             # Fix for some Java AWT applications (e.g. Android Studio),
             # use this if they aren't displayed properly:
             export _JAVA_AWT_WM_NONREPARENTING=1
-        ''
+        '';
     };
     waybar.enable = true;
 
