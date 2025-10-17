3267776588b2ffeea28c1a63a77e1c29a3d89aea
Seanwanq <seanwang313@outlook.com>
2025-10-17 15:11:49 +0200

    Modified ssh setting.

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -21,7 +21,7 @@
   };
 
   # Shell initialization to add keys
-  programs.zsh.initExtra = lib.mkAfter ''
+  programs.zsh.initContent = lib.mkAfter ''
     # Add SSH keys to agent if not already added
     if [ -n "$SSH_AUTH_SOCK" ] && [ -f ~/.ssh/"GitHub 2025" ]; then
       ssh-add -l | grep -q "GitHub 2025" || ssh-add ~/.ssh/"GitHub 2025" 2>/dev/null
