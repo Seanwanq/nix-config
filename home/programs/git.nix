87db983e5c85af660d664e327a9be3c9415709b1
Seanwanq <seanwang313@outlook.com>
2025-10-17 12:32:27 +0200

    Set up ssh agent.

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -10,20 +10,20 @@
 
     # 1Password SSH signing support (optional)
     # To enable, uncomment the following and set your signing key
-    # extraConfig = {
-    #   gpg = {
-    #     format = "ssh";
-    #   };
-    #   "gpg \"ssh\"" = {
-    #     program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-    #   };
-    #   commit = {
-    #     gpgsign = true;
-    #   };
-    #   user = {
-    #     signingKey = "ssh-ed25519 AAAAC3... your-key-here";
-    #   };
-    # };
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      user = {
+        signingKey = "ssh-ed25519 AAAAC3... your-key-here";
+      };
+    };
 
     # ... Other options ...
   };
