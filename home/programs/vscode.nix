108b007a64aa1ea740f55692c4857e811b2448cb
Seanwanq <seanwang313@outlook.com>
2025-10-19 12:54:59 +0200

    Removed all vscode config.

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -2,36 +2,6 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-
-    profiles = {
-      default = {
-        userSettings = {
-          # 基本编辑器设置
-          "editor.fontSize" = 14;
-          "editor.fontFamily" = "JetBrainsMono Nerd Font";
-          "editor.tabSize" = 2;
-          "editor.insertSpaces" = true;
-      
-          # 主题
-          "workbench.colorTheme" = "Default Dark+";
-      
-          # 终端设置
-          "terminal.integrated.fontSize" = 14;
-          "terminal.integrated.fontFamily" = "JetBrainsMono Nerd Font";
-      
-          # 文件设置
-          "files.autoSave" = "afterDelay";
-          "files.autoSaveDelay" = 1000;
-
-          # 中文输入法兼容性设置 (解决 Fcitx5 在 Electron 应用中拼音字母重复显示的问题)
-          # 启用 Wayland IME 支持
-          "window.titleBarStyle" = "custom";
-        };
-        enableExtensionUpdateCheck = false;
-        enableUpdateCheck = false;
-      };
-    };
-    
   };
 
   # 为 VSCode 设置 Electron 标志以支持 Wayland IME
